22a0c09d6083b9cafbd35c7031bc0ea3e346708b
sideness <sideness@hotmail.fr>
2015-01-15 16:48:04 +0100

    Réparation des erreurs GIT

--- Assets/script/BadScript.cs
+++ Assets/script/BadScript.cs
@@ -20,20 +20,7 @@ public class BadScript : MonoBehaviour {
 	
 	void OnGUI(){
 		if (displayGUI) {
-<<<<<<< HEAD
-			GUI.Label (new Rect (Screen.width / 2 - 50, Screen.width / 2 - 50, 100, 50), "Vous etes mort !");
-			if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.width / 2 - 25, 100, 50), "Recommencer")) {
-				SelectionScript.resetLevel();
-			}
-			if (GUI.Button (new Rect (Screen.width / 2 + 50, Screen.width / 2 - 25, 100, 50), "Menu")) {
-				Application.LoadLevel("menu");
-			}
-			if (GUI.Button (new Rect (Screen.width / 2 - 150, Screen.width / 2 - 25, 100, 50), "Quitter")) {
-				Application.Quit();
-			}
-=======
 			new ControllerMenu().display();
->>>>>>> b60d0d5fccc87d3945fa247b1ed56074303ac8c3
 		}
 
 	}
